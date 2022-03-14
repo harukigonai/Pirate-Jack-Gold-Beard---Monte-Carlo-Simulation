16180419f72d5060b860d3825f57fc6c48ec9c52
harukigonai <hg2541@columbia.edu>
2022-03-13 20:05:57 -0400

    Created First Version of Grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const gridNums = [];
+  for (let row = 0; row < 11; row++) {
+    for (let col = 0; col < 6; col++) {
+      gridNums.push({ row: row, col: col });
+    }
+  }
+
+  const rows = [];
+  for (let i = 0; i < 11; i++) {
+    rows.push(i);
+  }
+
+  const cols = [];
+  for (let i = 0; i < 6; i++) {
+    cols.push(i);
+  }
+
+  // console.log(gridNums, gridNums.map(<div></div>));
+  const [grid, setGrid] = useState(
+    rows.map((row) => (
+      <div key={row} className={`row row-${row}`}>
+        {cols.map((col) => (
+          <div
+            key={`${row} ${col}`}
+            className={`square square-${row}-${col}`}
+          ></div>
+        ))}
+      </div>
+    ))
+  );
+
+  console.log(grid);
+
+  const [max, setMax] = useState(1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="game">
+          <div className="score">Score</div>
+          <div className="nextIcons">
+            <div className="smallRow">
+              <div className="square"></div>
+            </div>
+            <div className="smallRow">
+              <div className="square"></div>
+            </div>
+          </div>
+          <div className="grid">{grid}</div>
+          <div className="unlockedIcons"></div>
+        </div>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +64,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
